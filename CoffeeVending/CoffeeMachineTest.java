cdfb9bff8a3baa86f576a515261ce35cc5df87d4
Sil1con <143268089+Sil1con@users.noreply.github.com>
2024-03-05 15:47:06 +0200

    Delete CoffeeVending directory

--- CoffeeVending/CoffeeMachineTest.java
+++
@@ -1,6 +0,0 @@
-public class CoffeeMachineTest {
-    public static void main (String[] args){
-        CoffeeMachine cf = new CoffeeMachine();
-        cf.receiveInput();
-    }
-}
--- CoffeeVending/beverage/Coffee/Espresso.java
+++
@@ -1,8 +0,0 @@
-package beverage.Coffee;
-
-public class Espresso extends Coffee {
-    final private double price = 2.5;
-    public double getPrice(){
-        return price;
-    }
-}
